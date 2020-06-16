601d903202748e556bf7644dd5af8bba17a75b48
JeonJunYeop <galid1@naver.com>
2020-06-16 15:21:30 +0900

    init

---
+++ src/main/java/com/galid/cicdjenkinstest/CicdJenkinsTestApplication.java
@@ -0,0 +1,13 @@
+package com.galid.cicdjenkinstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdJenkinsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdJenkinsTestApplication.class, args);
+    }
+
+}
