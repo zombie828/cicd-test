8a7b41f51fbf0538c31137fdb721f869528285f7
JeonJunYeop <galid1@naver.com>
2020-06-23 15:04:44 +0900

    1

--- src/main/java/com/galid/cicdjenkinstest/TestController.java
+++ src/main/java/com/galid/cicdjenkinstest/TestController.java
@@ -1,14 +1,14 @@
 package com.galid.cicdjenkinstest;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TestController {
 
     @GetMapping("/")
     public String test() {
-        return "test";
+        return "HI CI/CD Architecture!!!!";
     }
 
 
